61e3310bd5a60156d97b264141f46b8ba66dd14b
skkim <novaray@naver.com>
2024-02-23 18:07:13 +0900

    pinia 예제.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-quasar-ui'],
+  modules: ['nuxt-quasar-ui', '@pinia/nuxt'],
   quasar: {
     plugins: ['Notify'],
     config: {
--- pages/about.vue
+++ pages/about.vue
@@ -1,25 +1,22 @@
 <script setup lang="ts">
 const counter = useState<number>('counter', () => 1);
 const sameCounter = useState<number>('counter');
+
+const counterStore = useCounterStore();
+const { count, doubleCount } = storeToRefs(counterStore);
 </script>
 
 <template>
   <q-page padding>
     <div class="q-my-xl text-center">
       <PageTitle title="About" />
-      <pageDescription
-        description="Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다."
-      />
+      <pageDescription description="Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다." />
       <div class="column">
         <RouterLink to="/">RouterLink Home</RouterLink>
-        <RouterLink to="https://youtube.com/@gymcoding">
-          RouterLink Youtube
-        </RouterLink>
+        <RouterLink to="https://youtube.com/@gymcoding"> RouterLink Youtube </RouterLink>
         <br />
         <NuxtLink to="/">NuxtLink Home</NuxtLink>
-        <NuxtLink to="https://youtube.com/@gymcoding">
-          NuxtLink Youtube
-        </NuxtLink>
+        <NuxtLink to="https://youtube.com/@gymcoding"> NuxtLink Youtube </NuxtLink>
 
         <div class="q-gutter-y-sm q-mt-md">
           <div class="text-subtitle1 text-weight-bold">useState('counter')</div>
@@ -47,6 +44,18 @@ const sameCounter = useState<number>('counter');
             <q-btn
               label="clear"
               @click="clearNuxtState()"
+            />
+          </div>
+        </div>
+
+        <div class="q-gutter-y-sm q-mt-md">
+          <div class="text-subtitle1 text-weight-bold">useCounterStore</div>
+          <div>count: {{ count }}</div>
+          <div>doubleCount: {{ doubleCount }}</div>
+          <div>
+            <q-btn
+              label="increment"
+              @click="counterStore.increment()"
             />
           </div>
         </div>
