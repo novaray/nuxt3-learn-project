5197d9ac117f03eccb1fd9b06febb9da70842d23
skkim <novaray@naver.com>
2024-02-25 16:51:21 +0900

    model -> models 네이밍 변경.

--- server/api/courses/[courseSlug].get.ts
+++ server/api/courses/[courseSlug].get.ts
@@ -1,4 +1,4 @@
-import { getCourseDetails } from '~/server/model/course';
+import { getCourseDetails } from '~/server/models/course';
 
 export default defineEventHandler((event) => {
   const courseSlug = getRouterParam(event, 'courseSlug') as string;
--- server/api/courses/index.get.ts
+++ server/api/courses/index.get.ts
@@ -1,4 +1,4 @@
-import { getCourses } from '~/server/model/course';
+import { getCourses } from '~/server/models/course';
 
 export default defineEventHandler(() => {
   const courses = getCourses();
