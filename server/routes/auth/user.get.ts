ce668a8625b60e9bc00aac830abfc3e0fb1bc3df
skkim <novaray@naver.com>
2024-02-25 17:12:21 +0900

    server routes 설정.

---
+++ server/routes/auth/user.get.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler((event) => {
+  const userJsonString = getCookie(event, '__user');
+
+  if (!userJsonString) {
+    return { user: null };
+  }
+
+  const user = JSON.parse(userJsonString);
+  return {
+    user
+  };
+});
